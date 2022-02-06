707600a369a625344942e819e58599bde0669c2b
sergey-r88 <mcmouse88@icloud.com>
2022-02-06 15:50:36 +0200

    added new class - Elevator and chanched class Main

---
+++ src/com/company/Elevator.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Deque;
+
+public class Elevator {
+
+    private int floor;
+
+    private static int totalSeconds = 0;
+    private static int  previousFloor = -1;
+
+    public Elevator(int floor) {
+        this.floor = floor;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    @Override
+    public String toString() {
+        return "" + floor;
+    }
+
+    public static void timeMoveElevator(int floor) {
+        final int WAIT_DOORS_IN_SECOND = 10;
+        final int WAIT_MOVE_IN_SECOND = 5;
+        if (previousFloor != -1) {
+            totalSeconds += Math.abs((floor - previousFloor)) * WAIT_MOVE_IN_SECOND + WAIT_DOORS_IN_SECOND;
+        }
+        previousFloor = floor;
+    }
+
+    public static void printResult(Deque<Elevator> floors) {
+        timeMoveElevator(0);
+        System.out.println("Лифт проследовал по следующим этажам");
+        while (!floors.isEmpty()) {
+            System.out.print("этаж " + floors.poll() + " -> ");
+        }
+        System.out.println("Выход");
+        System.out.println("Время затраченное лифтом на маршрут = " + totalSeconds + " сек.");
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Deque<Integer> elevator = new ArrayDeque<>();
+        Deque<Elevator> floors = new ArrayDeque<>();
         int floor;
-        int waitDoorsInSeconds = 10;
-        int waitMoveInSeconds = 5;
-        int totalSeconds = 0;
-        int previousFloor = -1;
         while (true) {
             System.out.println("Ожидаю ввода этажа:");
             try {
@@ -30,19 +26,10 @@ public class Main {
             if (floor > 25 || floor < 0) {
                 System.out.println("Такого этажа нет в доме");
             } else {
-                elevator.offer(floor);
-                if (previousFloor != -1) {
-                    totalSeconds += Math.abs((floor - previousFloor)) * waitMoveInSeconds + waitDoorsInSeconds;
-                }
-                previousFloor = floor;
+                floors.offer(new Elevator(floor));
+                Elevator.timeMoveElevator(floor);
             }
-
         }
-        System.out.println("Лифт проследовал по следующим этажам");
-        while (!elevator.isEmpty()) {
-            System.out.print(elevator.poll() + " -> ");
-        }
-        System.out.println("Выход");
-        System.out.println("Время затраченное лифтом на маршрут = " + totalSeconds + " сек.");
+        Elevator.printResult(floors);
     }
 }
